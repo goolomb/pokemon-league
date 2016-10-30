3e2d067e3a2ca9ac4196271a3a53b0a3b1bb5c86
Martina Minátová <m.minatova@gmail.com>
2016-10-30 16:57:13 +0100

    Basic tests for Stadium DAO

---
+++ persistence/src/test/java/cz/muni/fi/pa165/dao/StadiumDaoTest.java
@@ -0,0 +1,115 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Stadium;
+import cz.muni.fi.pa165.entity.Trainer;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Tests for Stadium Data Access Object
+ *
+ * @author Martina Minatova
+ */
+public class StadiumDaoTest extends AbstractTestNGSpringContextTests {
+
+    StadiumDao stadiumDao = new StadiumDaoImpl();
+
+
+    @Test
+    public void testCreate() {
+        Stadium stadium = new Stadium();
+        stadium.setCity("Emerald");
+
+        Assert.assertNull(stadium.getId());
+
+        stadiumDao.create(stadium);
+        Stadium byId = stadiumDao.findById(stadium.getId());
+        Assert.assertEquals(stadium, byId);
+    }
+
+    @Test
+    public void testDelete() {
+        Stadium stadium = new Stadium();
+        stadium.setCity("Pallet");
+        Stadium toBeRemoved = new Stadium();
+        stadium.setCity("Viridian");
+
+        stadiumDao.create(stadium);
+        stadiumDao.create(toBeRemoved);
+
+        Assert.assertTrue(stadiumDao.findAll().size() == 2);
+
+        stadiumDao.delete(toBeRemoved);
+
+        Assert.assertTrue(stadiumDao.findAll().size() == 1);
+        Assert.assertEquals(stadium, stadiumDao.findById(stadium.getId()));
+        Assert.assertNull(stadiumDao.findById(toBeRemoved.getId()));
+
+    }
+
+    @Test
+    public void testFindAll() {
+        Stadium stadium1 = new Stadium();
+        stadium1.setCity("Lavender");
+        Stadium stadium2 = new Stadium();
+        stadium2.setCity("Fuchsia");
+
+        stadiumDao.create(stadium1);
+        stadiumDao.create(stadium2);
+
+        List<Stadium> all = stadiumDao.findAll();
+
+        Assert.assertTrue(all.size() == 2);
+        Assert.assertTrue(all.contains(stadium1));
+        Assert.assertTrue(all.contains(stadium2));
+    }
+
+    @Test
+    public void testFindByTrainer() {
+        Trainer trainer = new Trainer();
+        trainer.setFirstName("Ash");
+        trainer.setLastName("Ketchun");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2000,10,30); //YYYY,MM,DD
+        trainer.setBirthDate(calendar.getTime());
+
+        Stadium stadium1 = new Stadium();
+        stadium1.setCity("Saffron");
+        Stadium stadium2 = new Stadium();
+        stadium2.setCity("Pewter");
+        stadium2.setLeader(trainer);
+
+        stadiumDao.create(stadium1);
+        stadiumDao.create(stadium2);
+
+        Stadium byTrainer = stadiumDao.findByTrainer(trainer);
+        Assert.assertEquals(stadium2,byTrainer);
+    }
+
+    @Test
+    public void testUpdate() {
+        Stadium stadium1 = new Stadium();
+        stadium1.setCity("Red");
+
+        Stadium stadium2 = new Stadium();
+        stadium2.setCity("Blue");
+
+        stadiumDao.create(stadium1);
+        stadiumDao.create(stadium2);
+
+        stadium1.setCity("Yellow");
+
+        stadiumDao.update(stadium1);
+
+        Assert.assertEquals(stadium2, stadiumDao.findById(stadium2.getId()));
+        Stadium changedStadium = stadiumDao.findById(stadium1.getId());
+
+        Assert.assertEquals(stadium1.getCity(), changedStadium.getCity());
+        Assert.assertEquals(stadium1, changedStadium);
+    }
+
+}
