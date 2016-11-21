9cee2bd912a1a78e8c9cee544cc96c61e6791f57
Martina Minátová <m.minatova@gmail.com>
2016-11-21 23:02:52 +0100

    Added basic methods to pokemon service

--- service/src/main/java/cz/muni/fi/pa165/service/PokemonService.java
+++ service/src/main/java/cz/muni/fi/pa165/service/PokemonService.java
@@ -1,7 +1,24 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.entity.Pokemon;
+import cz.muni.fi.pa165.entity.Trainer;
+
+import java.util.List;
+
 /**
  * @author Martina Minatova
  */
 public interface PokemonService {
+
+    Pokemon findById(Long id);
+
+    void create(Pokemon pokemon);
+
+    void delete(Pokemon pokemon);
+
+    List<Pokemon> findAll();
+
+    List<Pokemon> findByTrainer(Trainer trainer);
+
+    Pokemon update(Pokemon pokemon);
 }
--- service/src/main/java/cz/muni/fi/pa165/service/PokemonServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/PokemonServiceImpl.java
@@ -1,7 +1,46 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.dao.PokemonDao;
+import cz.muni.fi.pa165.entity.Pokemon;
+import cz.muni.fi.pa165.entity.Trainer;
+
+import javax.inject.Inject;
+import java.util.List;
+
 /**
  * @author Martina Minatova
  */
 public class PokemonServiceImpl implements PokemonService {
+    @Inject
+    private PokemonDao pokemonDao;
+
+    @Override
+    public Pokemon findById(Long id) {
+        return pokemonDao.findById(id);
+    }
+
+    @Override
+    public void create(Pokemon pokemon) {
+        pokemonDao.create(pokemon);
+    }
+
+    @Override
+    public void delete(Pokemon pokemon) {
+        pokemonDao.delete(pokemon);
+    }
+
+    @Override
+    public List<Pokemon> findAll() {
+        return pokemonDao.findAll();
+    }
+
+    @Override
+    public List<Pokemon> findByTrainer(Trainer trainer) {
+        return pokemonDao.findByTrainer(trainer);
+    }
+
+    @Override
+    public Pokemon update(Pokemon pokemon) {
+        return pokemonDao.update(pokemon);
+    }
 }
