d4f2f1bfe85989536bd4fec0293292107752c178
Martina Minátová <m.minatova@gmail.com>
2016-11-21 22:08:08 +0100

    Added modules Service and API

---
+++ api/src/main/java/cz/muni/fi/pa165/facade/PokemonFacade.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.facade;
+
+/**
+ * @author Martina Minatova
+ */
+public interface PokemonFacade {
+}
---
+++ service/src/main/java/cz/muni/fi/pa165/service/PokemonServiceImpl.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.service;
+
+/**
+ * @author Martina Minatova
+ */
+public class PokemonServiceImpl implements PokemonService {
+}
