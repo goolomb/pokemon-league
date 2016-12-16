2a4a08820059e0c56c22e04d9edac5ed19c63e64
Martina Minátová <m.minatova@gmail.com>
2016-12-16 16:45:27 +0100

    fixed rest uris

--- web/src/main/java/cz/muni/fi/pa165/web/rest/ApiUris.java
+++ web/src/main/java/cz/muni/fi/pa165/web/rest/ApiUris.java
@@ -4,8 +4,8 @@ package cz.muni.fi.pa165.web.rest;
  * @author Martina Minatova
  */
 public class ApiUris {
-    public static final String ROOT_URI_POKEMON = "/pokemon";
-    public static final String ROOT_URI_BADGE = "/badge";
-    public static final String ROOT_URI_STADIUM = "/stadium";
-    public static final String ROOT_URI_TRAINER = "/trainer";
+    public static final String ROOT_URI_POKEMON = "/rest/pokemon";
+    public static final String ROOT_URI_BADGE = "/rest/badge";
+    public static final String ROOT_URI_STADIUM = "/rest/stadium";
+    public static final String ROOT_URI_TRAINER = "/rest/trainer";
 }
